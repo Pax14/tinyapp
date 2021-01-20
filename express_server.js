a9788e31f07bfa37c7b2b0a032e520d7c15921eb
Paxon Cheung <paxon.cheung@gmail.com>
2021-01-20 08:58:27 +0000

    updating deleting URLs

--- express_server.js
+++ express_server.js
@@ -28,6 +28,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
 app.post("/urls", (req, res) => {
   const short = generateRandomString();
   urlDatabase[short] = req.body.longURL;
